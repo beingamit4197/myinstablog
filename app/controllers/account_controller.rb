ba42700f8efea6a60a0e388908cacfd18d2d1c0a
Amit Rawat <beingamit4197@gmail.com>
2020-05-26 12:42:15 +0530

    My First Commit

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,13 @@
+class AccountController < ApplicationController
+
+    before_action :authenticate_account!
+
+    def index
+    #user feed
+    end
+
+    def show
+    #user profile
+    end
+
+end
